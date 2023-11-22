dff89578ab59b2f95904ea73808932314236842c
Manan Lodhia <mananlodhia@hotmail.com>
2023-11-22 16:57:16 +1100

    udpated structure

--- src/layouts/section.js
+++ src/layouts/section.js
@@ -1,9 +1,9 @@
-export const Section = ({ children, cols }) => {
+export const Section = ({ children, cols, customStyle }) => {
   return (
     <div className="container w-full mx-auto">
       <div className="max-w-full mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-5 ">
         <div
-          className={`grid ${
+          className={`grid ${customStyle} ${
             cols === 2 ? "lg:grid-cols-2" : cols === 3 ? "lg:grid-cols-3" : ""
           } sm:gap-x-32 items-center text-center`}
         >
--- src/pages/index.js
+++ src/pages/index.js
@@ -26,18 +26,82 @@ export default function Home() {
         height={"h-[33rem]"}
         logoWidth={"w-10/12"}
       />
-      <Section cols={1}>
-        <h1>Feature one</h1>
+      <Section cols={2}>
+        <div className="order-last text-start">
+          <h2 className="block mb-4 text-3xl font-semibold tracking-tight text-secondary sm:text-5xl font-display">
+            Events
+          </h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+        <div>
+          <img
+            // src="/runit.png"
+            className=""
+            alt="runit Logo"
+          />
+        </div>
       </Section>
       <Section cols={2}>
-        <div>
-          <h1>Feature two </h1>
+        <div className=" text-start">
+          <h2 className="block mb-4 text-3xl font-semibold tracking-tight text-secondary sm:text-5xl font-display">
+            Collaboration
+          </h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
         </div>
         <div>
-          <h1>Col Two</h1>
+          <img
+            // src="/runit.png"
+            className=""
+            alt="runit Logo"
+          />
         </div>
       </Section>
-      <Section cols={3}>
+      <Section cols={2}>
+        <div className="order-last  text-start">
+          <h2 className="block mb-4 text-3xl font-semibold tracking-tight text-secondary sm:text-5xl font-display">
+            Security & Profile
+          </h2>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+        <div>
+          <img
+            // src="/runit.png"
+            className=""
+            alt="runit Logo"
+          />
+        </div>
+      </Section>
+      {/*    <Section cols={3}>
         <div>
           <h1>Feature Three </h1>
         </div>
@@ -47,49 +111,48 @@ export default function Home() {
         <div>
           <h1>Three</h1>
         </div>
-      </Section>
-      <div className={styles.container}>
-        <main>
+      </Section> */}
+      <Section cols={1} customStyle={"bg-themeLight p-10 rounded-xl"}>
+        <div className="mx-auto">
           <h1 className={styles.title}>
-            Welcome to <a href="https://nextjs.org">Next.js!</a>
+            About <a href="https://nextjs.org">Us</a>
           </h1>
-          <p className="text-blue-500 font-bold">Powered by Tailwind</p>
 
-          <p className={styles.description}>
-            Get started by editing <code>pages/index.js</code>
-          </p>
+          <Section cols={2}>
+            <div>
+              <a href="https://nextjs.org/docs" className={styles.card}>
+                <h3>Julian Tjiong</h3>
+                <p>Find in-depth information about Next.js features and API.</p>
+              </a>
 
-          <div className={styles.grid}>
-            <a href="https://nextjs.org/docs" className={styles.card}>
-              <h3>Documentation &rarr;</h3>
-              <p>Find in-depth information about Next.js features and API.</p>
-            </a>
+              <a href="https://nextjs.org/learn" className={styles.card}>
+                <p>
+                  Learn about Next.js in an interactive course with quizzes!
+                </p>
+              </a>
+            </div>
 
-            <a href="https://nextjs.org/learn" className={styles.card}>
-              <h3>Learn &rarr;</h3>
-              <p>Learn about Next.js in an interactive course with quizzes!</p>
-            </a>
-
-            <a
-              href="https://github.com/vercel/next.js/tree/canary/examples"
-              className={styles.card}
-            >
-              <h3>Examples &rarr;</h3>
-              <p>Discover and deploy boilerplate example Next.js projects.</p>
-            </a>
-
-            <a
-              href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              className={styles.card}
-            >
-              <h3>Deploy &rarr;</h3>
-              <p>
-                Instantly deploy your Next.js site to a public URL with Vercel.
-              </p>
-            </a>
-          </div>
-        </main>
-      </div>
+            <div>
+              <a
+                href="https://github.com/vercel/next.js/tree/canary/examples"
+                className={styles.card}
+              >
+                <h3>Manan Lodhia</h3>
+                <p>Discover and deploy boilerplate example Next.js projects.</p>
+              </a>
+              <a
+                href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                className={styles.card}
+              >
+                <p>
+                  Instantly deploy your Next.js site to a public URL with
+                  Vercel.
+                </p>
+              </a>
+            </div>
+          </Section>
+        </div>
+      </Section>
 
       <SiteFooter />
     </div>

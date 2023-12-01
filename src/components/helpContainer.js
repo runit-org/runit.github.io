2c21c49753409f8ea102b1b0aeb0f58d11d492e5
Manan Lodhia <mananlodhia@hotmail.com>
2023-12-01 16:50:27 +1100

    styles

--- src/components/helpContainer.js
+++ src/components/helpContainer.js
@@ -1,7 +1,7 @@
 export default function HelpContainer({ title, children }) {
   return (
     <div>
-      <h2 className="sm:text-3xl">{title}</h2>
+      <h2 className="sm:text-3xl mb-1">{title}</h2>
       {children}
     </div>
   );
--- src/pages/help/(event).js
+++ src/pages/help/(event).js
@@ -15,165 +15,172 @@ import runitEventNotifications from "../../assets/runit-event-notifications.png"
 export default function Event() {
   return (
     //add title and children (modifiable)
-    <HelpContainer title={""}>
-      <div id="Browsing-Events">
-        <h2>Browsing Events</h2>
-        <p>
-          Navigate through the main page to explore events efficiently. The list
-          is organized chronologically, presenting event cards with pertinent
-          indicators. At the end of each scroll, you can find a 'show more'
-          button which is a type of loading feature we implemented for more
-          efficient rendering.
-        </p>
-        <Image
-          priority={true}
-          src={runitBrowseEventsPic}
-          alt="runit collab gif"
-          className="w-1/2"
-        />
-        Each card includes details like the event creator's username, creation
-        timestamp, join status, event title, and a WYSIWYG event description.
-        Keep an eye on event statuses—pending, underway, ended, or canceled. To
-        delve into detailed event information, click the 'Details' button for an
-        in-depth technical overview.
-        <Image
-          priority={true}
-          src={runitEventCardZoomed}
-          alt="runit collab gif"
-          className="w-1/2"
-        />
-      </div>
+    <HelpContainer title={"Events"}>
+      <h4 className="subtitle">Add some brief there about events</h4>
+      <div className="container mt-2 ">
+        <div id="Browsing-Events" className="anchor">
+          <h3 className="mb-0">Browsing Events</h3>
+          <p>
+            Navigate through the main page to explore events efficiently. The
+            list is organized chronologically, presenting event cards with
+            pertinent indicators. At the end of each scroll, you can find a
+            'show more' button which is a type of loading feature we implemented
+            for more efficient rendering.
+          </p>
+          <Image
+            priority={true}
+            src={runitBrowseEventsPic}
+            alt="runit collab gif"
+            className="w-1/2 my-8"
+          />
+          Each card includes details like the event creator's username, creation
+          timestamp, join status, event title, and a WYSIWYG event description.
+          Keep an eye on event statuses—pending, underway, ended, or canceled.
+          To delve into detailed event information, click the 'Details' button
+          for an in-depth technical overview.
+          <Image
+            priority={true}
+            src={runitEventCardZoomed}
+            alt="runit collab gif"
+            className="w-1/2 my-8"
+          />
+        </div>
 
-      <div id="Event-Suggestions">
-        <b>Event Suggestions</b>
-        On the left panel, you can see a list of suggested events curated from
-        eventfinda.com. These events are live and can be used as templates for
-        creating your own events. You can invite people to join you in attending
-        these events. The templates provide essential details such as a link,
-        time, and more, enabling you to efficiently use this information for
-        your event.
-        <Image
-          priority={true}
-          src={runitEventSuggestions}
-          alt="runit collab gif"
-          className="w-1/2"
-        />
-      </div>
+        <div id="Event-Suggestions" className="anchor">
+          <h3 className="mb-0">Event Suggestions</h3>
+          On the left panel, you can see a list of suggested events curated from
+          eventfinda.com. These events are live and can be used as templates for
+          creating your own events. You can invite people to join you in
+          attending these events. The templates provide essential details such
+          as a link, time, and more, enabling you to efficiently use this
+          information for your event.
+          <Image
+            priority={true}
+            src={runitEventSuggestions}
+            alt="runit collab gif"
+            className="w-1/2 my-8"
+          />
+        </div>
 
-      <div id="Creating-An-Event">
-        <b>Creating An Event</b>
-        Effortlessly craft your events on the home page with our user-friendly
-        interface. Utilize features like a concise event title, a convenient
-        dropdown for selecting date and time, and a versatile WYSIWYG editor.
-        The editor empowers you to seamlessly incorporate links, emphasize text
-        with bold characters, and tailor the content to your preferences. Your
-        event creation experience, simplified and customizable, starts here.
-        <Image
-          priority={true}
-          src={runitCreateEvent}
-          alt="runit collab gif"
-          className="w-1/2"
-        />
-      </div>
+        <div id="Creating-An-Event" className="anchor">
+          <h3 className="mb-0">Creating An Event</h3>
+          Effortlessly craft your events on the home page with our user-friendly
+          interface. Utilize features like a concise event title, a convenient
+          dropdown for selecting date and time, and a versatile WYSIWYG editor.
+          The editor empowers you to seamlessly incorporate links, emphasize
+          text with bold characters, and tailor the content to your preferences.
+          Your event creation experience, simplified and customizable, starts
+          here.
+          <Image
+            priority={true}
+            src={runitCreateEvent}
+            alt="runit collab gif"
+            className="w-1/2 my-8"
+          />
+        </div>
 
-      <div id="Joining-An-Event">
-        <b>Joining An Event</b>
-        Joining events is simple – just click the '+ Join' button on the top
-        right corner of each event card or in the event details. After clicking
-        join, the event creator receives a notification, giving them the option
-        to either accept or decline your request. Your status on each event will
-        be displayed on the join button once you've become involved.
-        <Image
-          priority={true}
-          src={runitJoinEvent}
-          alt="runit collab gif"
-          className="w-1/2"
-        />
-      </div>
+        <div id="Joining-An-Event" className="anchor">
+          <h3 className="mb-0">Joining An Event</h3>
+          Joining events is simple – just click the '+ Join' button on the top
+          right corner of each event card or in the event details. After
+          clicking join, the event creator receives a notification, giving them
+          the option to either accept or decline your request. Your status on
+          each event will be displayed on the join button once you've become
+          involved.
+          <Image
+            priority={true}
+            src={runitJoinEvent}
+            alt="runit collab gif"
+            className="w-1/2 my-8"
+          />
+        </div>
 
-      <div id="Event-Page">
-        <b>Event Page</b>
-        Clicking on 'Details' opens the event page, where you can explore the
-        full event description, comments, and the list of participants.
-        Additionally, you can post comments directly from this page. If you're
-        the event creator, you have the authority to accept or decline
-        participation requests right here.
-        <Image
-          priority={true}
-          src={runitEventPage}
-          alt="runit collab gif"
-          className="w-1/2"
-        />
-      </div>
+        <div id="Event-Page" className="anchor">
+          <h3 className="mb-0">Event Page</h3>
+          Clicking on 'Details' opens the event page, where you can explore the
+          full event description, comments, and the list of participants.
+          Additionally, you can post comments directly from this page. If you're
+          the event creator, you have the authority to accept or decline
+          participation requests right here.
+          <Image
+            priority={true}
+            src={runitEventPage}
+            alt="runit collab gif"
+            className="w-1/2 my-8"
+          />
+        </div>
 
-      <div id="Writing-Comments">
-        <b>Writing Comments</b>
-        After being accepted into an event or if you're the event owner,
-        contribute to the conversation by writing comments under the event. Your
-        comments will appear on the left panel of the event details. Utilize
-        features like tagging users with the @ symbol followed by their username
-        or a suggested username from event members. Once tagged, the target user
-        will be notified of your message. Notify everyone by using @everyone.
-        <Image
-          priority={true}
-          src={runitCommentMention}
-          alt="runit collab gif"
-          className="w-1/2"
-        />
-        You can also express appreciation by liking comments – simply click the
-        heart symbol under the comment, and the user will be notified if someone
-        likes their contribution.
-        <Image
-          priority={true}
-          src={runitLikeComment}
-          alt="runit collab gif"
-          className="w-1/2"
-        />
-      </div>
+        <div id="Writing-Comments" className="anchor">
+          <h3 className="mb-0">Writing Comments</h3>
+          After being accepted into an event or if you're the event owner,
+          contribute to the conversation by writing comments under the event.
+          Your comments will appear on the left panel of the event details.
+          Utilize features like tagging users with the @ symbol followed by
+          their username or a suggested username from event members. Once
+          tagged, the target user will be notified of your message. Notify
+          everyone by using @everyone.
+          <Image
+            priority={true}
+            src={runitCommentMention}
+            alt="runit collab gif"
+            className="w-1/2 my-8"
+          />
+          You can also express appreciation by liking comments – simply click
+          the heart symbol under the comment, and the user will be notified if
+          someone likes their contribution.
+          <Image
+            priority={true}
+            src={runitLikeComment}
+            alt="runit collab gif"
+            className="w-1/2 my-8"
+          />
+        </div>
 
-      <div id="Viewing-Members">
-        <b>Viewing Members</b>
-        Once you're in the event, explore a list of all accepted members by
-        clicking the Gravatar image circles under the event description. Each
-        member is displayed with their username and Gravatar images. From here,
-        you can conveniently navigate to their profiles for further details.
-        <Image
-          priority={true}
-          src={runitViewMembers}
-          alt="runit collab gif"
-          className="w-1/2"
-        />
-      </div>
+        <div id="Viewing-Members" className="anchor">
+          <h3 className="mb-0">Viewing Members</h3>
+          Once you're in the event, explore a list of all accepted members by
+          clicking the Gravatar image circles under the event description. Each
+          member is displayed with their username and Gravatar images. From
+          here, you can conveniently navigate to their profiles for further
+          details.
+          <Image
+            priority={true}
+            src={runitViewMembers}
+            alt="runit collab gif"
+            className="w-1/2 my-8"
+          />
+        </div>
 
-      <div id="Managing-Applicants">
-        <b>Managing Applicants</b>
-        As an event creator, efficiently manage applicants who've requested to
-        join your event. Find the applicant panel right under the create comment
-        section within the event details page. You have the flexibility to
-        either tick to accept a request or cross to decline it. Both actions
-        will notify the applicant. By accepting an applicant, you grant them the
-        ability to create comments under your event, facilitating their active
-        participation.
-        <Image
-          priority={true}
-          src={runitManageApplicants}
-          alt="runit collab gif"
-          className="w-1/2"
-        />
-      </div>
+        <div id="Managing-Applicants" className="anchor">
+          <h3 className="mb-0">Managing Applicants</h3>
+          As an event creator, efficiently manage applicants who've requested to
+          join your event. Find the applicant panel right under the create
+          comment section within the event details page. You have the
+          flexibility to either tick to accept a request or cross to decline it.
+          Both actions will notify the applicant. By accepting an applicant, you
+          grant them the ability to create comments under your event,
+          facilitating their active participation.
+          <Image
+            priority={true}
+            src={runitManageApplicants}
+            alt="runit collab gif"
+            className="w-1/2 my-8"
+          />
+        </div>
 
-      <div id="Notifications">
-        <b>Notifications</b>
-        Stay informed with notifications for key event actions, including
-        requests to join your event, your requests to join other users' events,
-        and the creation of comments. Access these notifications conveniently
-        located at the top right of your navigation bar.
-        <Image
-          priority={true}
-          src={runitEventNotifications}
-          alt="runit collab gif"
-          className="w-1/2"
-        />
+        <div id="Notifications" className="anchor">
+          <h3 className="mb-0">Notifications</h3>
+          Stay informed with notifications for key event actions, including
+          requests to join your event, your requests to join other users'
+          events, and the creation of comments. Access these notifications
+          conveniently located at the top right of your navigation bar.
+          <Image
+            priority={true}
+            src={runitEventNotifications}
+            alt="runit collab gif"
+            className="w-1/2 my-8"
+          />
+        </div>
       </div>
     </HelpContainer>
   );

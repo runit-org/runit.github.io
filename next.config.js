60d7db1b378dbbc7d799392120308c816ac77daf
Manan Lodhia <mananlodhia@hotmail.com>
2023-12-07 19:14:02 +1100

    config for dynamic routes

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: "/help/:help*",
+        source: "/help/:slug*",
         destination: "/help/[slug]",
       },
     ];

04959d082824c005765bbdd195dcd5418a09e8b6
Manan Lodhia <mananlodhia@hotmail.com>
2023-12-07 17:48:14 +1100

    updated base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /**
  * @type {import('next').NextConfig}
+ *
  */
+const isProd = (process.env.NODE_ENV = "production");
 const nextConfig = {
+  basePath: isProd ? "/runit.github.io" : "",
   output: "export",
   distDir: "dist",
   images: {
